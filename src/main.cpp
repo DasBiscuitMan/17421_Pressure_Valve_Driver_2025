00edc1f145adb2b9a14da2e25a890ef2d6de41c5
J.C00K <workmrcook@gmail.com>
2025-02-05 11:15:36 +0000

    Version 8 PID Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 /*  Title:            PPMS #17421 Motor Driver using PWM INPUT
     Author:           Joe Cook
     Equipment:        In BOM Folder
-    Notes:            Error has replaced position difference for PID control          
+    Notes:            Utilizes PID Library by Brett Beuregard          
 */
+//Librarys Included
+#include <PID_v1.h>
+
 //Variable Definitions
   
   //(PWM MEASURE) 
     //mark to space ratio measuring position variables
     #define pulsePin D5  // Define the input pin for the pulse signal
-    volatile long pulseStartTime = 0; // Variable to store the start time of the pulse (voltatile used to ensure updates in isr)
-    volatile long pulseEndTime = 0;   // Variable to store the end time of the pulse
-    volatile long pulseWidth = 0;     // Variable to store the pulse width (needs to be motor starting position CALIBRATE)
+    volatile double pulseStartTime = 0; // Variable to store the start time of the pulse (voltatile used to ensure updates in isr)
+    volatile double pulseEndTime = 0;   // Variable to store the end time of the pulse
+    volatile double pulseWidth = 0;     // Variable to store the pulse width (needs to be motor starting position CALIBRATE)
 
     volatile boolean debug_code = false; //If True PWM will print
     volatile boolean flag = 0; //used in switch to ensure calculation only occurs on low edge
@@ -18,22 +21,21 @@
     //averaging Variables
     volatile int numberOfSamplePulses = 500;
     volatile int pulseCount = 0;
-    volatile long pulseWidthTotal = 0;
-    volatile long averagePulseWidth = 0;
+    volatile double pulseWidthTotal = 0.0;
+    volatile double averagePulseWidth = 0.0;
    
     //mapping PWM Variables 
-    long mappedPWM = 0; //variable for mapping PWM value to motor position value
-    long maxWidth = 4000; //max us pulse can be (dependant on frequency used)
+    double mappedPWM = 0; //variable for mapping PWM value to motor position value
+    double maxWidth = 4000; //max us pulse can be (dependant on frequency used)
 
   //(MOTOR CONTROL)
-    //position variables
-    #define positionFeedback A0 //motor rotory feedback from 10 turn POT
-    int positionFeedbackReading; //to analog read the position feedback pin 
+    //error value
+    int error = 0; //int so that no decimals 
     //motor output variables
     int clockwise = D10; //powers clockwise pin on DRV8876 H-Bridge
     int anticlockwise = D9; //powers anticlockwise pin on DRV8876 H-Bridge
     //Speed Variables
-    int speed = 0; 
+    double speed = 0; 
     //LED Indicators
     #define blueLED A2
     #define redLED A3
@@ -42,33 +44,29 @@
     #define setpointMet D7
 
   //SLIDING WINDOW for Feedback Potentiometer and PWM Measurement
+    //position variables
+    #define positionFeedback A0 //motor rotory feedback from 10 turn POT
+    double positionFeedbackReading; //to analog read the position feedback pin 
     //Time delay
-    unsigned long storedTimeStamp = 0;
+    double storedTimeStamp = 0;
     //values for sliding window
     const unsigned int numReadings = 100; //ammount of sampling values in the array
     //array with size
-    unsigned int potValues[numReadings]; 
+    double potValues[numReadings]; 
     unsigned int i = 0; //pointer for if function
     //value for summing the array
-    unsigned int potSum = 0; 
+    double potSum = 0; 
     //value for average of array
-    unsigned int averageFeedbackValue = 0; 
+    double averageFeedbackValue = 0; 
 
-  //PID 
-  //Gains
-  float kp = 0.1; 
-  float ki = 0.001; //
-  float kd = 0;
-  float u = 0; //PID calculation
-  //delta t variables
-  float deltaT;
-  int currentTime;
-  int previousTime;
-  //error variables
-  long error;
-  float previousError;
-  float eIntegral;
-  float eDerivative;
+  //PID Variables
+  double kp = 1;
+  double ki = 0;
+  double kd = 0;
+
+  double Setpoint, Feedback, Output;
+
+  PID myPID(&Feedback, &Output, &Setpoint, kp, ki, kd, DIRECT);
 
 void setup() {
   //(PWM MEASURE)
@@ -92,20 +90,62 @@ void setup() {
 
   //OVERSHOOT CHECKING
     pinMode(setpointMet, OUTPUT); //measure using logic analyser then measure 
+  
+  //INITIALIZE PID
+    myPID.SetMode(AUTOMATIC);
+    //set the output limits (default is 0,255)
+    myPID.SetOutputLimits(0, 4096);
+
 }
 void loop() {
 
   pwmCalculate();
 
   slidingWindow();
-  
-  PID();
 
   motorControl();
-    
-  serialPrinting();
+
+  excelPlotting();
+  //serialPrinting();
   
     }
+
+
+//(MOTOR CONTROL)
+void motorControl(){
+  //Set PID Values to Values defined within program
+  Feedback = averageFeedbackValue;
+  Setpoint = mappedPWM;
+  //error value
+  error = Setpoint - Feedback;
+  //set tunings and compute PID protocol
+  myPID.SetTunings(kp, ki, kd);
+  myPID.Compute();
+
+  //speed limit
+  speed = abs(Output); //sets speed always a positive number
+  if(speed > 255) speed = 255; //upper limit
+  if(speed < 110) speed = 110; //lower limit (110 no valve 160 with valve)
+  
+  //set direction and speed according to PID value
+  if (error == 0){
+    digitalWrite(clockwise, 1);
+    digitalWrite(anticlockwise, 1);
+    setColour(255, 0, 255); //green
+  } 
+  if (error > 0){
+    myPID.SetControllerDirection(DIRECT);
+    analogWrite(clockwise, speed);
+    analogWrite(anticlockwise, 0);
+    setColour(255, 0, 100); //torquoise
+  } 
+  if (error < 0){ 
+    myPID.SetControllerDirection(REVERSE);
+    analogWrite(clockwise, 0);
+    analogWrite(anticlockwise, speed);
+    setColour(0, 255, 255); //red
+  }
+}
 
 //ISR For mark measurement on both edges USING IF
 void changingEdgeISR(){  
@@ -130,50 +170,6 @@ void pwmCalculate(){
       pulseWidthTotal = 0;   
     }
   mappedPWM = map(averagePulseWidth, 0, maxWidth, 0, 4096);
-}
-
-//(MOTOR CONTROL)
-void motorControl(){
-
-  //speed limit
-  speed = fabs(u); //sets speed always a positive number
-  if(speed > 255) speed = 255; //upper limit
-  if(speed < 120) speed = 200; //lower limit
-  
-  //set direction and speed according to PID value
-  if (u == 0){
-    digitalWrite(clockwise, 1);
-    digitalWrite(anticlockwise, 1);
-    setColour(255, 0, 255); //green
-  } 
-  if (u > 0){
-    analogWrite(clockwise, speed);
-    analogWrite(anticlockwise, 0);
-    setColour(255, 0, 100); //torquoise
-  } 
-  if (u < 0){ 
-    analogWrite(clockwise, 0);
-    analogWrite(anticlockwise, speed);
-    setColour(0, 255, 255); //red
-  }
-
-}
-
-
-//PID Control
-void PID(){
-  //recording deltaT
-  currentTime = micros();
-  deltaT = ((float) currentTime - previousTime) / 1.0e6; //converts to seconds
-  //Proportional, Integral and Derivative
-  error = mappedPWM - averageFeedbackValue; //proportional error
-  eIntegral = eIntegral + error * deltaT; //integral value
-  eDerivative = error - previousError / deltaT; //delta value
-  //PID Calculation
-  u = (kp * error) + (ki * eIntegral) + (kd * eDerivative);
-  //setting values for next iteration
-  previousTime = currentTime;
-  previousError = error;
 }
 
 //AVERAGING
@@ -207,18 +203,33 @@ void setColour(int redValue, int greenValue, int blueValue) {
   analogWrite(blueLED, blueValue);
 }
 
+void excelPlotting(){
+  
+  //Delay function (alter to micros if needed)
+  static unsigned long storedTimeStamp = 0;
+  unsigned long currentTime = millis(); //stamp of time since arduino started up
+  if(currentTime >= storedTimeStamp + 200){ //alter number for delay time wanted
+    storedTimeStamp = currentTime; //stores current value to be compared with next measured value
+  Serial.print(micros() / 1e6);
+  Serial.print(" ");
+  Serial.print(Setpoint);
+  Serial.print(" ");
+  Serial.println(Feedback);
+  }
+  
+}
 void serialPrinting(){
 
     Serial.print(" average pulseWidth ");
     Serial.print(averagePulseWidth);
-    Serial.print(" mapped pulseWidth ");
-    Serial.print(mappedPWM);
-    Serial.print(" motor position ");
-    Serial.print(averageFeedbackValue);
+    Serial.print(" Setpoint ");
+    Serial.print(Setpoint);
+    Serial.print(" Feedback ");
+    Serial.print(Feedback);
     Serial.print(" error ");
     Serial.print(error);
-    Serial.print(" PID Value ");
-    Serial.print(u);
+    Serial.print(" Output ");
+    Serial.print(Output);
     Serial.print(" Speed ");
     Serial.println(speed);
 }
