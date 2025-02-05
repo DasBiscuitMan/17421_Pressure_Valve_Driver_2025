8c5b66acf9035505313d9a1a59f41fb342b7bb5b
J.C00K <workmrcook@gmail.com>
2025-02-05 10:37:52 +0000

    Version 4

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,16 @@
     volatile long pulseStartTime = 0; // Variable to store the start time of the pulse (voltatile used to ensure updates in isr)
     volatile long pulseEndTime = 0;   // Variable to store the end time of the pulse
     volatile long pulseWidth = 0;     // Variable to store the pulse width (needs to be motor starting position CALIBRATE)
-    //flags to indicate 1 cycle
-    volatile long flag = 0; 
+
+    volatile boolean debug_code = false; //If True PWM will print
+    volatile boolean flag = 0; //used in switch to ensure calculation only occurs on low edge
+
+    //averaging Variables
+    volatile int numberOfSamplePulses = 1000;
+    volatile int pulseCount = 0;
+    volatile long pulseWidthTotal = 0;
+    volatile long averagePulseWidth = 0;
+    
     //mapping PWM Variables 
     int mappedPWM = 0; //variable for mapping PWM value to motor position value
     int maxWidth = 2000; //max us pulse can be (dependant on frequency used)
@@ -23,17 +31,20 @@
     //motor output variables
     int clockwise = D10; //powers clockwise pin on DRV8876 H-Bridge
     int anticlockwise = D9; //powers anticlockwise pin on DRV8876 H-Bridge
+    //Switch statement Variables
+    #define ON_TARGET 1
+    #define CLOCKWISE 2
+    #define ANTICLOCKWISE 3
+    int switchValue = 0;
     //LED Indicators
     #define blueLED A2
     #define redLED A3
     #define greenLED D4
-
     //tolerancing variables
-    int toleranceValue = 10; //value for tolerance so can be altered in main program
+    int toleranceValue = 100; //value for tolerance so can be altered in main program
     int tolerance = toleranceValue; //actual plus minus value for tolerancing 
-    int flagTolerance = 0; //flag to ensure value is met before tolerance is used
-
-  
+    signed int positionDifference;
+    signed int dynamicTolerance;
 
   //SLIDING WINDOW for Feedback Potentiometer and PWM Measurement
     //Time delay
@@ -42,14 +53,11 @@
     const unsigned int numReadings = 100; //ammount of sampling values in the array
     //array with size
     unsigned int potValues[numReadings]; 
-    unsigned int pwmValues[numReadings]; 
     unsigned int i = 0; //pointer for if function
     //value for summing the array
     unsigned int potSum = 0; 
-    unsigned int pwmSum = 0; 
     //value for average of array
     unsigned int averageFeedbackValue = 0; 
-    unsigned int averagePWMValue = 0;
 
 void setup() {
   //(PWM MEASURE)
@@ -72,10 +80,8 @@ void setup() {
     Serial.begin(9600); //CANNOT BE USED WHEN NOT USING PC (MESSES UP)
 }
 void loop() {
-//(PWM MEASURE)
-  pwmMeasure();
 
-  SlidingWindow();
+  slidingWindow();
   
   motorControl();
     
@@ -85,69 +91,72 @@ void loop() {
 
 //ISR For mark measurement on both edges USING IF
 void changingEdgeISR(){  
-  if(digitalRead(pulsePin) == 1 & flag == 0){ //adding flag makes sequential so that high pin is read first
-  pulseStartTime = micros(); //measures stamp in microseconds
-  flag = 1; //checks whether pulse gone high
-  }
-  if(digitalRead(pulsePin) == 0 & flag == 1){
-  pulseEndTime = micros(); //measures stamp in microseconds
-  flag = 2; //checks whether pulse gone low
-  }
-}
-
-//(PWM MEASURE)
-void pwmMeasure(){
-    if(flag == 2){
-        pulseWidth = pulseEndTime - pulseStartTime; //subtracts stamp on rising edge from stamp on falling edge
-        flag = 0;
+ flag = (digitalRead(pulsePin));
+ switch(flag){
+  
+    case (1) : pulseStartTime = micros(); break;
+               
+    case (0) : pulseEndTime = micros();
+               pulseWidth = pulseEndTime - pulseStartTime; 
+               pulseCount++;
+               pulseWidthTotal += pulseWidth;
+               if(pulseCount >= numberOfSamplePulses) 
+               { pulseCount = 0;
+                 averagePulseWidth = pulseWidthTotal/numberOfSamplePulses; 
+                 if (debug_code) Serial.println(averagePulseWidth);
+                 pulseWidthTotal = 0;}  
+               break;            
     }
-  //mapping procedure for pulse measurement  
-  mappedPWM = map(pulseWidth, 0, maxWidth, 0, 4096); //4096 is 12 bit max ADC value 2^12
 }
-
 
 //(MOTOR CONTROL)
 void motorControl(){
-    //logic system to control motor rotation and brake 
-    //brake if motor in correct position
-    if((averagePWMValue - averageFeedbackValue) <= tolerance){
-      tolerance = toleranceValue; //tolerance set again to allow for deadband
+    // Calculate the difference between the current position and the target position
+    positionDifference = (averagePulseWidth - averageFeedbackValue);
+    
+    // Calculate the tolerance dynamically based on the position difference
+    dynamicTolerance = max(toleranceValue, abs(positionDifference) / 2); // Ensure dynamicTolerance is always positive
+    
+    // Logic system to control motor rotation and brake 
+    if (abs(positionDifference) <= dynamicTolerance) { // If within deadband
+        switchValue = ON_TARGET; // Brake if motor in correct position
+     } 
+    else if (positionDifference > 0) { // If motor is higher than correct position
+          switchValue = CLOCKWISE; // Rotate clockwise
+    } 
+    else if (positionDifference < 0) { // If motor is lower than correct position
+          switchValue = ANTICLOCKWISE; // Rotate anticlockwise
+    }
+    
+
+  switch (switchValue) {
+    case(ON_TARGET):
+      tolerance = dynamicTolerance; //tolerance set again to allow for deadband
       digitalWrite(clockwise, 1);
       digitalWrite(anticlockwise, 1);
       setColour(200, 1, 127); //0 = high
-    }
-    //rotate clockwise if motor is lower than correct position
-    if(averagePWMValue > (averageFeedbackValue + tolerance)){
-      tolerance = 0; //set to 0 to stop deadband being in place in movement
+      break;
+
+    case(CLOCKWISE):
       digitalWrite(clockwise, 1);
       digitalWrite(anticlockwise, 0);
       setColour(0, 1, 255); //0 = high
-    }
-    //rotate clockwise if motor is lower than correct position
-    if(averagePWMValue < (averageFeedbackValue - tolerance)){
-      tolerance = 0; //set to 0 to stop deadband being in place in movement
+      break;
+    
+    case(ANTICLOCKWISE):
       digitalWrite(clockwise, 0);
       digitalWrite(anticlockwise, 1);
       setColour(0, 1, 255); //0 = high
-    }
+    break;
+
+    default:
+      digitalWrite(clockwise, 1);
+      digitalWrite(anticlockwise, 1);
+      setColour(225, 0, 255); //0 = high
+  }
 }
 
-void serialPrinting(){
-    Serial.print("pulseWidth ");
-    Serial.print(pulseWidth);
-    Serial.print(" mapped pulseWidth ");
-    Serial.print(mappedPWM);
-    Serial.print(" average pulseWidth ");
-    Serial.print(averagePWMValue);
-    Serial.print(" motor position ");
-    Serial.print(averageFeedbackValue);
-    Serial.print(" clockwise ");
-    Serial.print(digitalRead(clockwise));
-    Serial.print(" anticlockwise ");
-    Serial.println(digitalRead(anticlockwise));
-}
-
-void SlidingWindow(){
+void slidingWindow(){
     //Delay function (alter to micros if needed)
   unsigned long currentMicros = micros();//stamp of time since arduino started up
 
@@ -156,20 +165,17 @@ void SlidingWindow(){
     
     // Remove the oldest value from the sum
     potSum -= potValues[i];
-    pwmSum -= pwmValues[i];
+    
     
     // Read the new value and add it to the sum
     potValues[i] = analogRead(positionFeedback);
     potSum += potValues[i];
-    pwmValues[i] = mappedPWM;
-    pwmSum += pwmValues[i];
     
     // Move to the next position in the array
     i = (i + 1) % numReadings;
 
     // Calculate the average
     averageFeedbackValue = potSum / numReadings;
-    averagePWMValue = pwmSum / numReadings;
 
   }
 }
@@ -180,5 +186,21 @@ void setColour(int redValue, int greenValue, int blueValue) {
   analogWrite(blueLED, blueValue);
 }
 
+void serialPrinting(){
+    Serial.print("pulseWidth ");
+    Serial.print(pulseWidth);
+    Serial.print(" average pulseWidth ");
+    Serial.print(averagePulseWidth);
+    Serial.print(" motor position ");
+    Serial.print(averageFeedbackValue);
+    Serial.print(" switchValue ");
+    Serial.print(switchValue);
+    Serial.print(" dynamic tolerance ");
+    Serial.print(dynamicTolerance);
+    Serial.print(" position difference ");
+    Serial.println(positionDifference);
+
+
+}
 
 
